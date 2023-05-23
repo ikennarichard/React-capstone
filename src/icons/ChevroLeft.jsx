a13d3b32d46620b770fd53cbc16642b19eabf6f7
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-23 02:41:23 -1100

    Add left arrow icon

---
+++ src/icons/ChevroLeft.jsx
@@ -0,0 +1,7 @@
+export default function ChevroLeft() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="w-6 h-6" width={20}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+    </svg>
+  );
+}
