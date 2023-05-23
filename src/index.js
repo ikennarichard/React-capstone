18c07776dcab5c47719c0cfc574edbbf4f18f9f5
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-23 02:36:05 -1100

    Add global state to the app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import store from './redux/store';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
