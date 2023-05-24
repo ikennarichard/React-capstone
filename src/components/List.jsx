5d93cb43f6ff2ca40d1448d47b34eca2c93c5e68
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-23 20:33:21 -1100

    Update list items

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import styles from './List.module.css';
 
 export default function List({
-  co, no, no2, o3, so2, aqi,
+  co, no, no2, o3, so2, aqi, city, pm25,
 }) {
   const [rating, setRating] = useState('');
 
@@ -25,23 +26,35 @@ export default function List({
   return (
     <li>
       <div>
+        <h2 className={styles.subHeading}>
+          { `${city} State` }
+          <br />
+          <small>{`Air Quality Index: ${aqi} (${rating})`}</small>
+        </h2>
+        <p className={styles.listItem}>
+          Particulates PM2.5
+          <span>{` ${pm25} μg/m3`}</span>
+        </p>
+
         <p>
-          {`Carbon: ${co}μg/m3`}
+          Carbon:
+          <span>{`${co} μg/m3`}</span>
         </p>
         <p>
-          {`Nitrogen Dioxide: ${no2}μg/m3`}
+          Nitrogen Dioxide:
+          <span>{`${no2} μg/m3`}</span>
         </p>
         <p>
-          {`Nitrogen Monoxide: ${no}μg/m3`}
+          Nitrogen Monoxide:
+          <span>{`${no} μg/m3`}</span>
         </p>
         <p>
-          {`Sulphur Dioxide: ${so2}μg/m3`}
+          Sulpher Dioxide:
+          <span>{`${so2} μg/m3`}</span>
         </p>
         <p>
-          {`Ozone: ${o3}μg/m3`}
-        </p>
-        <p>
-          {`Air Quality Index: ${aqi} (${rating})`}
+          Ozone:
+          <span>{`${o3} μg/m3`}</span>
         </p>
       </div>
     </li>
@@ -55,4 +68,6 @@ List.propTypes = {
   o3: PropTypes.number.isRequired,
   so2: PropTypes.number.isRequired,
   aqi: PropTypes.number.isRequired,
+  city: PropTypes.string.isRequired,
+  pm25: PropTypes.number.isRequired,
 };
